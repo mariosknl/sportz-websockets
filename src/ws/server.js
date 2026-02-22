ba9d8329b02bbc2d673175c17c9b16b67ad790e6
Marios Kanellopoulos <mariosknl@outlook.com>
2026-02-22 09:52:05 +0200

    implement arcjet security

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,5 @@
 import {WebSocket, WebSocketServer} from 'ws';
+import {wsArcjet} from "../arcjet.js";
 
 function sendJson(socket, payload) {
     if (socket.readyState !== WebSocket.OPEN) return;
@@ -21,7 +22,26 @@ export function attachWebSocketServer(server) {
         maxPayload: 1024 * 1024
     });
 
-    wss.on('connection', (socket) => {
+    wss.on('connection', async(socket, req) => {
+
+        if(wsArcjet) {
+            try {
+                const decision = await wsArcjet.protect(req);
+
+                if (decision.isDenied()) {
+                    const code = decision.reason.isRateLimit() ? 1013 : 1008;
+                    const reason = decision.reason.isRateLimit() ? 'Rate limit exceeded' : 'Access Denied';
+
+                    socket.close(code, reason)
+                    return;
+                }
+            }catch (e) {
+                console.error('WS connection error', e);
+                socket.close(1011, 'Server security error');
+                return;
+            }
+        }
+
         socket.isAlive = true;
         socket.on('pong', () => {
             socket.isAlive = true;
@@ -41,7 +61,7 @@ export function attachWebSocketServer(server) {
             })
         }, 3000)
 
-        ws.on('close', () => clearInterval(interval));
+        wss.on('close', () => clearInterval(interval));
 
 
     function broadcastMatchCreated(match) {
